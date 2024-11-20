a6dbe3e7cfdef09d5a5ed3f013827bc9f0d44bd7
Indra Khatiwada <khatiwadai024@gmail.com>
2024-11-20 12:19:27 +0545

    Basic workflow

--- prisma/clear-db.sql
+++
@@ -1,7 +0,0 @@
--- Clear all tables while respecting foreign key constraints
-TRUNCATE TABLE "Session" CASCADE;
-TRUNCATE TABLE "Account" CASCADE;
-TRUNCATE TABLE "User" CASCADE;
-TRUNCATE TABLE "Promise" CASCADE;
-TRUNCATE TABLE "Party" CASCADE;
-TRUNCATE TABLE "VerificationToken" CASCADE;
--- scripts/reset-postgres.sql
+++
@@ -1,9 +0,0 @@
--- Deallocate all prepared statements
-DO $$ 
-DECLARE 
-    r RECORD;
-BEGIN
-    FOR r IN (SELECT name FROM pg_prepared_statements) LOOP
-        EXECUTE 'DEALLOCATE "' || r.name || '"';
-    END LOOP;
-END $$;
